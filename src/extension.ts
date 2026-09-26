a2cc714e13cea29cbc1c482d85c958895175342a
Jerald Bucud <jeraldbucud@gmail.com>
2026-09-27 00:28:53 +1000

    feat: wire project persistence storage

--- src/extension.ts
+++ src/extension.ts
@@ -5,12 +5,17 @@ import { WorkspaceContextService } from "./context/workspaceContext";
 import { ProgressiveHintEngine } from "./learning/hints";
 import { NextStepService } from "./learning/nextSteps";
 import { ProjectIntelligenceService } from "./project/projectIntelligence";
+import { ProjectPersistenceService } from "./project/projectPersistence";
+import { createVsCodeProjectPersistenceAdapter } from "./project/vscodeProjectPersistence";
 import { LearningModeViewProvider } from "./ui/learningModeView";
 
 export function activate(context: vscode.ExtensionContext): void {
+  const projectPersistence = new ProjectPersistenceService(
+    createVsCodeProjectPersistenceAdapter(context.globalStorageUri)
+  );
   const controller = new CodingSenseiController(
     new WorkspaceContextService(),
-    new ProjectIntelligenceService(),
+    new ProjectIntelligenceService(undefined, projectPersistence),
     new ProgressiveHintEngine(),
     new NextStepService(),
     new LearningModeViewProvider(context.extensionUri)
